f4cb306a783d2b481be0949d313ccbba57133950
Babak <babakshofficial@yahoo.co.uk>
2019-10-12 05:57:44 +0330

    Using Two plots in a figure

--- sample_data.py
+++ sample_data.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plot
 
-N = 10
+N = 1000
 mean = [0, 0]
 cov = [[1, 0], [0, 1]]
 sdata = np.random.multivariate_normal(mean, cov, N)
@@ -12,20 +12,27 @@ second_column = []
 for i in range(N):
     first_column.append(sdata[:][i][0])
     second_column.append(sdata[:][i][1])
-plot.scatter(first_column, second_column)
+
+#plot.scatter(first_column, second_column)
 plot.show()
 
-cov2 = [[1, 0], [0, 1]]
+cov2 = [[1, 0.9], [0.9, 1]]
 sdata2 = np.random.multivariate_normal(mean, cov2, N)
-
-z2 = []
 
 first_column2 = []
 second_column2 = []
 for i in range(N):
     first_column2.append(sdata2[:][i][0])
     second_column2.append(sdata2[:][i][1])
+#plot.scatter(first_column2, second_column2)
+
+plot.figure('hello')
+plot.subplot(121)
+plot.scatter(first_column, second_column)
+plot.subplot(122)
 plot.scatter(first_column2, second_column2)
+
+plot.show()
 
 result = np.cov(sdata, sdata2)
 print(result)
