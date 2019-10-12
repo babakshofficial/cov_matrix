d096d8df07a99714e2a611a0c21e8d7ae5321f77
Babak <babakshofficial@yahoo.co.uk>
2019-10-12 19:20:45 +0330

    Comments Added

--- calculate_cov.py
+++ calculate_cov.py
@@ -1,13 +1,15 @@
-
+# calculating Mean
 def mean(x):
     return sum(x) / len(x)
 
+#calculating covariance between the two same sized sets
 def cov(x, y):
     x_mean = mean(x)
     y_mean = mean(y)
     data = [(x[i] - x_mean) * (y[i] - y_mean) for i in range(len(x))]
     return sum(data) / (len(data) - 1)
 
+#creating covariance matrix
 def make_cov_matrix(sdata):
     cov_matrix = []
     for i in range(len(sdata)):
