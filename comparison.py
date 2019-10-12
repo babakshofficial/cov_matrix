9040f6287ba64820737de6329e10342ebb8652a7
Babak <babakshofficial@yahoo.co.uk>
2019-10-12 20:10:35 +0330

    comments added for comaparison.py

--- comparison.py
+++ comparison.py
@@ -1,10 +1,16 @@
+
+#comparing size and each value of the two matries
+#peak is modified because of rounding around floating point
 def comparison(listA, listB, peak):
+    #whole length comparison
     if len(listA) != len(listB):
         return 0
+    #each inner list length comparison
     for i in range(len(listA)):
         for j in range(len(listB)):
             if len(listA[i]) != len(listB[j]):
                 return 0
+    #each value length comparison rounded around floating point by a peak
     for i in range(len(listA)):
         for j in range(len(listB)):
             if round(listA[i][j],peak) != round(listB[i][j],peak):
