aac858ce0b52f7153aab0f38f9430878d0735d9e
Babak <babakshofficial@yahoo.co.uk>
2019-10-12 19:19:09 +0330

    Comments Added

--- sample_data.py
+++ sample_data.py
@@ -2,12 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plot
 import calculate_cov as COV
 
+#initialization
 N = 1000
 mean = [0, 0]
+
+#FIRST MATRIX
 cov = [[1, 0], [0, 1]]
+
+#multivariate_normal implementation includes cov
+#normal Implementation includes standard Deviation
+#produce N sample data based on multivariate normal (Gausian) distribution
 sdata = np.random.multivariate_normal(mean, cov, N)
 
-z = []
+#converting class array type to simple list
+#and disjoint columns
 first_column = []
 second_column = []
 for i in range(N):
@@ -17,6 +25,7 @@ for i in range(N):
 #plot.scatter(first_column, second_column)
 plot.show()
 
+#SECOND MATRIX
 cov2 = [[1, 0.9], [0.9, 1]]
 sdata2 = np.random.multivariate_normal(mean, cov2, N)
 
@@ -27,20 +36,32 @@ for i in range(N):
     second_column2.append(sdata2[:][i][1])
 #plot.scatter(first_column2, second_column2)
 
+
+#draw two scatter plots in a single figure
 plot.figure('Scatter Plots')
 plot.subplot(121)
 plot.scatter(first_column, second_column)
 plot.subplot(122)
 plot.scatter(first_column2, second_column2)
 
-plot.show()
+#plot.show()
 
-print ("The numpy Generated Covariance Matrix \n")
+#comparison between numpy result and our simple way result
+half = int(N/2)
+print ("The numpy Generated Values: \n")
 result = np.cov(sdata2)
-#print(result)
-print ("\nOur non-numpy Generated Covariance Matrix \n")
+print ("1. numpy[last][last] = " + str(result[-1][-1]))
+print ("2. numpy[N/2][N/2] = " + str(result[half][half]))
+print ("3. numpy[10][20] = " + str(result[10][20]))
+print ("4. numpy[first][last] = " + str(result[0][-1]))
+print ("5. numpy[last][first] = " + str(result[-1][0]))
+
+print ("\nOur non-numpy Generated Values \n")
 non_numpy = COV.make_cov_matrix(sdata2)
-#for i in range(len(non_numpy)):
-#    print(non_numpy[i])
-#print
+print ("1. non-numpy[last][last] = " + str(non_numpy[-1][-1]))
+print ("2. non-numpy[N/2][N/2] = " + str(non_numpy[half][half]))
+print ("3. non-numpy[10][20] = " + str(non_numpy[10][20]))
+print ("4. non-numpy[first][last] = " + str(non_numpy[0][-1]))
+print ("5. non-numpy[last][first] = " + str(non_numpy[-1][0]))
+
 
