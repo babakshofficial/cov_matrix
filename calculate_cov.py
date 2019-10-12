a2d83d39fe807aff4e20eab83cf8ad168cd93e4d
Babak <babakshofficial@yahoo.co.uk>
2019-10-12 18:55:42 +0330

    Creating Covariance Matrix without python Libraries!

---
+++ calculate_cov.py
@@ -0,0 +1,19 @@
+
+def mean(x):
+    return sum(x) / len(x)
+
+def cov(x, y):
+    x_mean = mean(x)
+    y_mean = mean(y)
+    data = [(x[i] - x_mean) * (y[i] - y_mean)
+            for i in range(len(x))]
+    return sum(data) / (len(data) - 1)
+
+def make_cov_matrix(sdata):
+    cov_matrix = []
+    for i in range(len(sdata)):
+        tmp = []
+        for j in range(len(sdata)):
+            tmp.append(cov(sdata[i],sdata[j]))
+        cov_matrix.append(tmp)
+    return cov_matrix
--- sample_data.py
+++ sample_data.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plot
+import calculate_cov as COV
 
 N = 1000
 mean = [0, 0]
@@ -26,7 +27,7 @@ for i in range(N):
     second_column2.append(sdata2[:][i][1])
 #plot.scatter(first_column2, second_column2)
 
-plot.figure('hello')
+plot.figure('Scatter Plots')
 plot.subplot(121)
 plot.scatter(first_column, second_column)
 plot.subplot(122)
@@ -34,5 +35,12 @@ plot.scatter(first_column2, second_column2)
 
 plot.show()
 
-result = np.cov(sdata, sdata2)
-print(result)
+print ("The numpy Generated Covariance Matrix \n")
+result = np.cov(sdata2)
+#print(result)
+print ("\nOur non-numpy Generated Covariance Matrix \n")
+non_numpy = COV.make_cov_matrix(sdata2)
+#for i in range(len(non_numpy)):
+#    print(non_numpy[i])
+#print
+
